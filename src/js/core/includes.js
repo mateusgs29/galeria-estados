707f7e071ad9f37e1717d3e2ed628cb8eb5be217
Mateus Gomes <mateus.gs1999@gmail.com>
2021-04-09 23:38:31 -0300

    adicionado funcao para ler o arquvo json e montar os cards

--- src/js/core/includes.js
+++ src/js/core/includes.js
@@ -26,4 +26,30 @@ function loadIncludes(parent) {
     })
 } 
 
+function loadCards() {
+    $.getJSON('/data.json', function(result) {
+        const cards = []
+        $.each(result, function(i, e) {
+            const div = $('<div>').addClass(['col-12', 'col-md-6', 'col-lg-3'])
+            const card = $('<div>').addClass(['card', 'mx-auto', 'mb-4']).css('width', '15rem')
+            const image = $('<img>')
+                .addClass(['card-img-top', 'border-bottom', 'border-secondary'])
+                .attr('src', e.url)
+                .attr('alt', `Bandeira ${e.nome}`)
+            const cardBody = $('<div>').addClass('card-body')
+            const h5 = $('<h5>').addClass('card-title').html(e.nome)
+            const h6 = $('<h6>').addClass(['card-subtitle', 'mb-2', 'text-muted']).html(`Região ${e.regiao}`)
+
+            cardBody.append(h5).append(h6)
+            card.append(image).append(cardBody)
+            div.append(card)
+
+            cards.push(div)
+        })
+
+        $('[wm-includeCards]').html(cards)
+    })
+}
+
 loadIncludes()
+loadCards()
